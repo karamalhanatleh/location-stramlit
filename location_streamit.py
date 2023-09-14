aaab2ef2b5cda79fc33c2a5d1c82d1b6b7f0f8ac
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-15 00:02:11 +0300

    Update location_streamit.py

--- location_streamit.py
+++ location_streamit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+# Custom CSS to center-align the title
 title_style = """
     <style>
         .title {
@@ -14,9 +15,7 @@ st.markdown(title_style, unsafe_allow_html=True)
 # Center-aligned title
 st.markdown("<h1 class='title'>حي العمور</h1>", unsafe_allow_html=True)
 
-# Display the Google Maps iframe
+# Display the Google Maps iframe with responsive dimensions
 st.components.v1.html(
-    '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.5836358763736!2d35.778246574557436!3d32.21544821247477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151c85e548325d0b%3A0x7bba8399a6eedfd6!2z2K3ZiiDYp9mE2LnZhdmI2LE!5e0!3m2!1sen!2sjo!4v1694723145386!5m2!1sen!2sjo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-    width=350,
-    height=480,
+    '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.5836358763736!2d35.778246574557436!3d32.21544821247477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151c85e548325d0b%3A0x7bba8399a6eedfd6!2z2K3ZiiDYp9mE2LnZhdmI2LE!5e0!3m2!1sen!2sjo!4v1694723145386!5m2!1sen!2sjo" width="100%" height="70vh" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
 )
